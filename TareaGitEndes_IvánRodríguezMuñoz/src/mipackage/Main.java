5e3a39d23021ce2dc17d765f4cba363b2f4fde80
iván <romuvan13@gmail.com>
2025-04-25 11:35:23 +0200

    Primer Commit

---
+++ TareaGitEndes_IvánRodríguezMuñoz/src/mipackage/Main.java
@@ -0,0 +1,19 @@
+package mipackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		HolaMundoENDES();
+		
+		
+	}
+	
+	public static void HolaMundoENDES() {
+		
+		System.out.println("Hola, Mundo!");
+		
+	}
+	
+}
---
+++ TareaGitEndes_IvánRodríguezMuñoz/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TareaGitEndes_IvánRodríguezMuñoz {
+}
