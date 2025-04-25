55600ffb16d49802792ddfe3f39eb09d89734c5a
iván <romuvan13@gmail.com>
2025-04-25 12:22:14 +0200

    Bucle añadido

--- TareaGitEndes_IvánRodríguezMuñoz/src/mipackage/Main.java
+++ TareaGitEndes_IvánRodríguezMuñoz/src/mipackage/Main.java
@@ -1,20 +1,29 @@
 package mipackage;
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		int input = -1;
+		Scanner sc = new Scanner(System.in);
 		
-		
-		HolaMundoENDES(9);
-		HolaMundoENDES(15);
-		
-		
+		while (true) {
+			
+			System.out.println("Dime un número: (0 para salir del bucle)");
+			input = sc.nextInt();
+			HolaMundoENDES(input);
+			
+			if (input == 0)
+				return;
+			
+		}
 	}
 	
 	public static void HolaMundoENDES(int numero) {
 		
-		if (numero > 10)
+		if (numero != 0)
 			System.out.println("Hola, Mundo!");
 		else
 			System.out.println("Adiós, Mundo!");
